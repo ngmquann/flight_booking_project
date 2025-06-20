8c5084477f2d8fbe96af118a257ce9cd9bdcb934
Quan <mminhquann6996@gmail.com>
2025-06-20 09:48:29 +0700

    initial backend

---
+++ backend/flight_booking_project/src/test/java/com/javaweb/flight_booking_project/FlightBookingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaweb.flight_booking_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
